f2627e555285e18fb7c1e78541a077411398ded1
devnandito <fhersa@gmail.com>
2021-02-19 14:56:37 -0300

    add validation system

--- app/controllers/client.go
+++ app/controllers/client.go
@@ -5,9 +5,10 @@ import (
 	"github.com/revel/revel"
 	"errors"
 	models "docusys/app/models"
-	// "strconv"
+	"strconv"
 	"github.com/vcraescu/go-paginator/v2"
 	"github.com/vcraescu/go-paginator/v2/adapter"
+	"time"
 )
 
 type Client struct {
@@ -25,6 +26,7 @@ func (c Client) Index() revel.Result {
 }
 
 func (c Client) List(page int) revel.Result {
+	h2 := "GO CRUD WITH POSTGRES - SHOW RECORDS"
 	clients := []models.Client{}
 	q := DB.Order("id desc").Find(&clients)
 	err := q.Error
@@ -81,32 +83,35 @@ func (c Client) List(page int) revel.Result {
 	// }
 	// fmt.Println(s)
 	
-	return c.Render(clients, p, s, current)
+	return c.Render(clients, p, s, current, h2)
 }
 
+func (c Client) Create(client *models.Client) revel.Result {
+	client.Validate(c.Validation)
 
-func (c Client) Create() revel.Result {
-
-	first_name := c.Params.Form.Get("first_name")
-	last_name := c.Params.Form.Get("last_name")
-	ci := c.Params.Form.Get("ci")
-
-	c.Validation.Required(first_name).Message("Nombre es requerido")
-	c.Validation.Required(last_name).Message("Apellido es requerido")
-	c.Validation.Required(ci).Message("Apellido es requerido")
+	first_name := c.Params.Form.Get("client.First_name")
+	last_name := c.Params.Form.Get("client.Last_name")
+	ci := c.Params.Form.Get("client.Ci")
+	birthday := c.Params.Form.Get("client.Birthday")
+	sex := c.Params.Form.Get("client.Sex")
 
 	if c.Validation.HasErrors() {
 		c.Validation.Keep()
 		c.FlashParams()
-		return c.Redirect(Client.Index)
+		return c.Redirect(Client.List)
 	}
 
-	client := models.Client{
+	const SQL_DATE_FORMAT = "2006-01-02"
+	t, _ := time.Parse(SQL_DATE_FORMAT, birthday)
+
+	data := models.Client{
 		First_name: first_name,
 		Last_name: last_name,
 		Ci: ci,
+		Birthday: t,
+		Sex: sex,
 	}
-	res := DB.Create(&client)
+	res := DB.Create(&data)
 
 	if res.Error != nil {
 		return c.RenderError(errors.New("Record Create failure." + res.Error.Error()))
@@ -114,9 +119,49 @@ func (c Client) Create() revel.Result {
 	return c.Redirect("/clients")
 }
 
-func (c Client) Delete() revel.Result {
+// func (c Client) Create() revel.Result {
+// 	first_name := c.Params.Form.Get("first_name")
+// 	last_name := c.Params.Form.Get("last_name")
+// 	ci := c.Params.Form.Get("ci")
+// 	birthday := c.Params.Form.Get("birthday")
+// 	sex := c.Params.Form.Get("sex")
 
-	id := c.Params.Route.Get("id")
+// 	c.Validation.Required(first_name).Message("Nombre es requerido")
+// 	c.Validation.Required(last_name).Message("Apellido es requerido")
+// 	c.Validation.Required(ci).Message("Apellido es requerido")
+// 	c.Validation.Required(ci).Message("Apellido es requerido")
+// 	c.Validation.Required(birthday).Message("Fecha de nacimiento es requerido")
+// 	c.Validation.Required(sex).Message("Sexo es requerido")
+// 	c.Validation.MinSize(sex, 1).Message("Debe ingresar solamente las iniciales")
+
+// 	if c.Validation.HasErrors() {
+// 		c.Validation.Keep()
+// 		c.FlashParams()
+// 		return c.Redirect(Client.List)
+// 	}
+
+// 	const SQL_DATE_FORMAT = "2006-01-02"
+// 	t, _ := time.Parse(SQL_DATE_FORMAT, birthday)
+
+// 	data := models.Client{
+// 		First_name: first_name,
+// 		Last_name: last_name,
+// 		Ci: ci,
+// 		Birthday: t,
+// 		Sex: sex,
+// 	}
+// 	res := DB.Create(&data)
+
+// 	if res.Error != nil {
+// 		return c.RenderError(errors.New("Record Create failure." + res.Error.Error()))
+// 	}
+// 	return c.Redirect(Client.List)
+// }
+
+func (c Client) Delete(id int) revel.Result {
+
+	// id := c.Params.Route.Get("id")
+	// id := c.Params.Form.Get("id")
 	client := []models.Client{}
 	res := DB.Delete(&client, id)
 	// res := DB.Unscoped().Delete(&clients, id) // delete permanently
@@ -125,10 +170,10 @@ func (c Client) Delete() revel.Result {
 		return c.RenderError(errors.New("Record Delete failure." + res.Error.Error()))
 	}
 
-	return c.Redirect("/clients")
+	return c.Redirect(Client.List)
 }
 
-func (c Client) Edit(id int) revel.Result {
+func (c Client) Edit(id int, first_name, last_name, ci, sex string, birthday time.Time) revel.Result {
 
 	client := []models.Client{}
 	res := DB.Find(&client, id)
@@ -137,37 +182,159 @@ func (c Client) Edit(id int) revel.Result {
 	if err != nil {
 		return c.RenderError(errors.New("Record not Found"))
 	}
-	return c.Render(client)
+
+	for _, i := range(client){
+		first_name = i.First_name
+		last_name = i.Last_name
+		ci = i.Ci
+		sex = i.Sex
+		birthday = i.Birthday
+	}
+
+	return c.Render(client, id, first_name, last_name, sex, ci, birthday)
 }
 
-func (c Client) Update(id int) revel.Result {
+func (c Client) Update(client *models.Client) revel.Result {
+	client.Validate(c.Validation)
 	
-	// pk := c.Params.Form.Get("id")
-	// id, _ := strconv.Atoi(pk)
+	pk := c.Params.Form.Get("client.ID")
+	id, _ := strconv.Atoi(pk)
 	
-	first_name := c.Params.Form.Get("first_name")
-	last_name := c.Params.Form.Get("last_name")
-	ci := c.Params.Form.Get("ci")
-
-	c.Validation.Required(first_name).Message("Nombre es requerido")
-	c.Validation.Required(last_name).Message("Apellido es requerido")
-	c.Validation.Required(ci).Message("Apellido es requerido")
+	first_name := c.Params.Form.Get("client.First_name")
+	last_name := c.Params.Form.Get("client.Last_name")
+	ci := c.Params.Form.Get("client.Ci")
+	birthday := c.Params.Form.Get("client.Birthday")
+	sex := c.Params.Form.Get("client.Sex")
+	
+	t, _ := time.Parse("2006-01-02", birthday)
+	fmt.Println("Time form:", t)
 
 	if c.Validation.HasErrors() {
 		c.Validation.Keep()
 		c.FlashParams()
-		return c.Redirect(Client.Index)
+		return c.Redirect(Client.Edit, id, first_name, last_name, sex, ci, birthday)
 	}
 
-	client := models.Client{}
+	// client1 := models.Client{}
 	// res := DB.Model(&client).Where("id = ?", id).Update("first_name", first_name)
 	// res := DB.Model(&client).Where("id = ?", id).Updates(map[string]interface{}{"first_name": first_name, "last_name": last_name, "ci": ci})
-	res := DB.Model(&client).Where("id = ?", id).Updates(models.Client{First_name: first_name, Last_name: last_name, Ci: ci})
+	res := DB.Model(&client).Where("id = ?", id).Updates(models.Client{First_name: first_name, Last_name: last_name, Ci: ci, Birthday: t, Sex: sex})
 
 	if res.Error != nil {
 		return c.RenderError(errors.New("Record Create failure." + res.Error.Error()))
 	}
-	return c.Redirect("/clients")
+	return c.Redirect(Client.List)
+
+}
+
+// func (c Client) Update(client *models.Client) revel.Result {
+	
+// 	pk := c.Params.Form.Get("id")
+// 	id, _ := strconv.Atoi(pk)
+	
+// 	first_name := c.Params.Form.Get("first_name")
+// 	last_name := c.Params.Form.Get("last_name")
+// 	ci := c.Params.Form.Get("ci")
+// 	birthday := c.Params.Form.Get("birthday")
+// 	sex := c.Params.Form.Get("sex")
+
+// 	c.Validation.Required(first_name).Message("Nombre es requerido")
+// 	c.Validation.Required(last_name).Message("Apellido es requerido")
+// 	c.Validation.Required(ci).Message("Apellido es requerido")
+// 	c.Validation.Required(birthday).Message("Fecha de nacimiento es requerido")
+// 	c.Validation.Required(sex).Message("Sexo es requerido")
+// 	c.Validation.MinSize(sex, 1).Message("Debe ingresar solamente las iniciales")
+	
+// 	t, _ := time.Parse("2006-01-02", birthday)
+// 	fmt.Println("Time form:", t)
+
+// 	if c.Validation.HasErrors() {
+// 		c.Validation.Keep()
+// 		c.FlashParams()
+// 		return c.Redirect(Client.Edit, id)
+// 	}
+
+// 	// client1 := models.Client{}
+// 	// res := DB.Model(&client).Where("id = ?", id).Update("first_name", first_name)
+// 	// res := DB.Model(&client).Where("id = ?", id).Updates(map[string]interface{}{"first_name": first_name, "last_name": last_name, "ci": ci})
+// 	res := DB.Model(&client).Where("id = ?", id).Updates(models.Client{First_name: first_name, Last_name: last_name, Ci: ci, Birthday: t, Sex: sex})
+
+// 	if res.Error != nil {
+// 		return c.RenderError(errors.New("Record Create failure." + res.Error.Error()))
+// 	}
+// 	return c.Redirect("/clients")
+// }
+
+func (c Client) Search() revel.Result {
+	h2 := "GO CRUD WITH POSTGRES - SEARCH RECORD"
+	notFound := "No existe registro"
+	client := []models.Client{}
+	first_name := c.Params.Form.Get("first_name")
+	last_name := c.Params.Form.Get("last_name")
+	document := c.Params.Form.Get("document")
+
+	if first_name == "" && last_name == "" && document == ""{
+		res := DB.Find(&client)
+		err := res.Error
+
+		if err != nil {
+			return c.RenderError(errors.New("Record not Found"))
+		}
+		// return c.Render(client, h2)
+		
+	} else if first_name == "" && last_name == "" {
+		res := DB.Where("ci LIKE ?", document+"%").Find(&client)
+		// res := DB.Find(&client, "ci LIKE ?", document)
+		err := res.Error
+
+		if err != nil {
+			return c.RenderError(errors.New("Record not Found"))
+		}
+		// return c.Render(client, h2)
+	} else if last_name == "" && document == "" {
+		res := DB.Where("first_name LIKE ?", first_name+"%").Find(&client)
+		err := res.Error
+
+		if err != nil {
+			return c.RenderError(errors.New("Record not Found"))
+		}
+		// return c.Render(client, h2)
+	} else if first_name == "" && document == "" {
+		res := DB.Where("last_name LIKE ?", last_name+"%").Find(&client)
+		err := res.Error
+
+		if err != nil {
+			return c.RenderError(errors.New("Record not Found"))
+		}
+		// return c.Render(client, h2)
+	} else if first_name == "" {
+		res := DB.Where("last_name LIKE ? OR ci LIKE ?", last_name+"%", document+"%").Find(&client)
+		err := res.Error
+
+		if err != nil {
+			return c.RenderError(errors.New("Record not Found"))
+		}
+		// return c.Render(client, h2)
+	} else if last_name == "" {
+		res := DB.Where("first_name LIKE ? OR ci LIKE ?", first_name, document).Find(&client)
+		err := res.Error
+
+		if err != nil {
+			return c.RenderError(errors.New("Record not Found"))
+		}
+		// return c.Render(client, h2)
+	} else if document == "" {
+		res := DB.Where("first_name LIKE ? OR last_name LIKE ?", first_name, last_name).Find(&client)
+		err := res.Error
+
+		if err != nil {
+			return c.RenderError(errors.New("Record not Found"))
+		}
+		// return c.Render(client, h2)
+	}
+
+	return c.Render(client, h2, notFound)
+	// db.Where("updated_at > ?", lastWeek).Find(&users)
 }
 
 // func (c Client) Update() revel.Result {
--- app/models/client.go
+++ app/models/client.go
@@ -2,11 +2,36 @@ package models
 
 import (
 	"gorm.io/gorm"
+	"time"
+	"github.com/revel/revel"
 )
 
 type Client struct {
 	gorm.Model
 	First_name string `sql:"size:255" json:"first_name"`
 	Last_name string `sql:"size:255" json:"last_name"`
-	Ci string `sql:"size:255" json:"ci"`
+	Ci string `sql:"size:20" json:"ci"`
+	// Birthday string `sql:"timestamptz" json:"birthday"`
+	Birthday time.Time `sql:"timestamptz" json:"birthday"`
+	Sex string `sql:size:1 json:"sex"`
+	Nationality string `sql:size:140 json:"nationality"`
+	Des_type string `sql:size:140 json: "des_type"`
+	Code1 string `sql:size:20 json:"code1"`
+	Code2 string `sql:size:20 json:"code2"`
+	Code3 string `sql:size:20 json:"code3"`
+	Direction string `sql:size:20 json:"direction"`
+	Phone string `sql:size:10 json:"phone"`
+}
+
+func (c Client) BirthdayDateStr() string {
+	return c.Birthday.Format("2006-01-02")
+}
+
+func (client *Client) Validate(v *revel.Validation) {
+	v.Required(client.First_name).Message("* El nombre es requerido")
+	v.Required(client.Last_name).Message("* El apellido es requerido")
+	v.Required(client.Ci).Message("* La cedula es requerido")
+	v.Required(client.Birthday).Message("* La fecha de nacimiento es requerido")
+	v.Required(client.Sex).Message("* El sexo es requerido")
+	v.MaxSize(client.Sex, 1).Message("* Ingrese solo la inicial")
 }
