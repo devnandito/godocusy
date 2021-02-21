c9791b284346b70895f75967fa547a6a6585bfb7
Diego Saldivar <fhersa@gmail.com>
2021-02-20 23:37:03 -0300

    create search and pagination

--- app/controllers/client.go
+++ app/controllers/client.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	models "docusys/app/models"
 	"strconv"
+	"strings"
 	"github.com/vcraescu/go-paginator/v2"
 	"github.com/vcraescu/go-paginator/v2/adapter"
 	"time"
@@ -15,12 +16,177 @@ type Client struct {
 	*revel.Controller
 }
 
-func (c Client) List(page int) revel.Result {
+func (c Client) Index() revel.Result {
+	h2 := "GO CRUD WITH POSTGRES - SEARCH"
+	return c.Render(h2)
+}
+
+func (c Client) Create(client *models.Client) revel.Result {
+	client.Validate(c.Validation)
+
+	first_name := c.Params.Form.Get("client.First_name")
+	last_name := c.Params.Form.Get("client.Last_name")
+	ci := c.Params.Form.Get("client.Ci")
+	birthday := c.Params.Form.Get("client.Birthday")
+	sex := c.Params.Form.Get("client.Sex")
+
+	if c.Validation.HasErrors() {
+		c.Validation.Keep()
+		c.FlashParams()
+		return c.Redirect(Client.ListPage)
+	}
+
+	const SQL_DATE_FORMAT = "2006-01-02"
+	t, _ := time.Parse(SQL_DATE_FORMAT, birthday)
+
+	data := models.Client{
+		First_name: first_name,
+		Last_name: last_name,
+		Ci: ci,
+		Birthday: t,
+		Sex: sex,
+	}
+	res := DB.Create(&data)
+
+	if res.Error != nil {
+		return c.RenderError(errors.New("Record Create failure." + res.Error.Error()))
+	}
+	return c.Redirect(Client.ListPage)
+}
+
+func (c Client) Edit(id int, first_name, last_name, ci, sex string, birthday time.Time) revel.Result {
+
+	client := []models.Client{}
+	res := DB.Find(&client, id)
+	err := res.Error
+
+	if err != nil {
+		return c.RenderError(errors.New("Record not Found"))
+	}
+
+	for _, i := range(client){
+		first_name = i.First_name
+		last_name = i.Last_name
+		ci = i.Ci
+		sex = i.Sex
+		birthday = i.Birthday
+	}
+
+	return c.Render(id, first_name, last_name, sex, ci, birthday)
+}
+
+func (c Client) Update(client *models.Client) revel.Result {
+	client.Validate(c.Validation)
+	
+	pk := c.Params.Form.Get("client.ID")
+	id, _ := strconv.Atoi(pk)
+	
+	first_name := c.Params.Form.Get("client.First_name")
+	last_name := c.Params.Form.Get("client.Last_name")
+	ci := c.Params.Form.Get("client.Ci")
+	birthday := c.Params.Form.Get("client.Birthday")
+	sex := c.Params.Form.Get("client.Sex")
+	
+	t, _ := time.Parse("2006-01-02", birthday)
+	fmt.Println("Time form:", t)
+
+	if c.Validation.HasErrors() {
+		c.Validation.Keep()
+		c.FlashParams()
+		return c.Redirect(Client.Edit, id, first_name, last_name, sex, ci, birthday)
+	}
+
+	res := DB.Model(&client).Where("id = ?", id).Updates(models.Client{First_name: first_name, Last_name: last_name, Ci: ci, Birthday: t, Sex: sex})
+
+	if res.Error != nil {
+		return c.RenderError(errors.New("Record Create failure." + res.Error.Error()))
+	}
+	return c.Redirect(Client.ListPage)
+
+}
+
+func (c Client) Delete(id int) revel.Result {
+
+	client := []models.Client{}
+	res := DB.Delete(&client, id)
+
+	if res.Error != nil {
+		return c.RenderError(errors.New("Record Delete failure." + res.Error.Error()))
+	}
+
+	return c.Redirect(Client.ListPage)
+}
+
+func (c Client) Search() revel.Result {
+	h2 := "GO CRUD WITH POSTGRES - SEARCH RECORD"
+	notFound := "No existe registro"
+	clients := []models.Client{}
+	first_name := strings.ToUpper(c.Params.Form.Get("first_name"))
+	last_name := strings.ToUpper(c.Params.Form.Get("last_name"))
+	document := c.Params.Form.Get("document")
+	var count int64
+
+	if first_name == "" && last_name == "" && document == ""{
+		res := DB.Order("last_name asc").Find(&clients).Count(&count)
+		err := res.Error
+
+		if err != nil {
+			return c.RenderError(errors.New("Record not Found"))
+		}
+		
+	} else if first_name == "" && last_name == "" {
+		res := DB.Order("ci asc").Where("ci LIKE ?", document+"%").Find(&clients).Count(&count)
+		err := res.Error
+
+		if err != nil {
+			return c.RenderError(errors.New("Record not Found"))
+		}
+	} else if last_name == "" && document == "" {
+		res := DB.Where("first_name LIKE ?", first_name+"%").Find(&clients).Count(&count)
+		err := res.Error
+
+		if err != nil {
+			return c.RenderError(errors.New("Record not Found"))
+		}
+	} else if first_name == "" && document == "" {
+		res := DB.Order("last_name asc").Where("last_name LIKE ?", last_name+"%").Find(&clients).Count(&count)
+		err := res.Error
+
+		if err != nil {
+			return c.RenderError(errors.New("Record not Found"))
+		}
+	} else if first_name == "" {
+		res := DB.Order("last_name asc").Where("last_name LIKE ? OR ci LIKE ?", last_name+"%", document+"%").Find(&clients).Count(&count)
+		err := res.Error
+
+		if err != nil {
+			return c.RenderError(errors.New("Record not Found"))
+		}
+	} else if last_name == "" {
+		res := DB.Order("last_name asc").Where("first_name LIKE ? OR ci LIKE ?", first_name+"%", document+"%").Find(&clients).Count(&count)
+		err := res.Error
+
+		if err != nil {
+			return c.RenderError(errors.New("Record not Found"))
+		}
+	} else if document == "" {
+		res := DB.Order("last_name asc, first_name asc").Where("first_name LIKE ? OR last_name LIKE ?", first_name+"%", last_name+"%").Find(&clients).Count(&count)
+		err := res.Error
+
+		if err != nil {
+			return c.RenderError(errors.New("Record not Found"))
+		}
+	}
+
+	return c.Render(clients, h2, notFound, count)
+}
+
+func (c Client) ListPage(page int) revel.Result {
 	h2 := "GO CRUD WITH POSTGRES - SHOW RECORDS"
 	clients := []models.Client{}
 	q := DB.Order("id desc").Find(&clients)
 	err := q.Error
-	p := paginator.New(adapter.NewGORMAdapter(q), 3)
+	p := paginator.New(adapter.NewGORMAdapter(q), 10)
 	if page == 0 {
 		p.SetPage(1) // set page actual
 		page = 1
@@ -31,235 +197,15 @@ func (c Client) List(page int) revel.Result {
 		panic(err)
 	}
 
-	for _, client := range clients {
-		fmt.Println(client.First_name)
-	}
-
-	hasPage, _ := p.HasPages()
-		fmt.Println("Exists pages:", hasPage)
-	
-	hasNext, _ := p.HasNext()
-		fmt.Println("Exists next:", hasNext)
-	
-	hasPrev, _ := p.HasPrev()
-		fmt.Println("Exists prev:", hasPrev)
-	
-	next, _ := p.NextPage()
-		fmt.Println("Next page:", next)
+	totalPage, _ := p.PageNums()
 	
 	current, _ := p.Page()
-		fmt.Println("Current page:", current)
-	
-	prev, _ := p.PrevPage()
-	 	fmt.Println("Prev page:", prev)
-	
-	totalPage, _ := p.PageNums()
-	 	fmt.Println("Total page:", totalPage)
-
-	nums, _ := p.Nums()
-	 	fmt.Println("Total records:", nums)
 	
 	var s []int
 
 	for i:=1; i<= totalPage; i++ {
 	 	s = append(s, i)
 	}
-	fmt.Println(s)
-
-	// var s []string
-
-	// for i:=1; i<= totalPage; i++ {
-	//  	s = append(s, strconv.Itoa(i))
-	// }
-	// fmt.Println(s)
 	
 	return c.Render(clients, p, s, current, h2)
-}
-
-func (c Client) Index() revel.Result {
-	clients := []models.Client{}
-	res := DB.Order("id desc").Find(&clients)
-	err := res.Error
-	if err != nil {
-		return c.RenderError(errors.New("Record not Found"))
-	}
-	return c.Render(clients)
-}
-
-func (c Client) Create() revel.Result {
-	first_name := c.Params.Form.Get("first_name")
-	last_name := c.Params.Form.Get("last_name")
-	ci := c.Params.Form.Get("ci")
-	birthday := c.Params.Form.Get("birthday")
-	sex := c.Params.Form.Get("sex")
-
-	c.Validation.Required(first_name).Message("Nombre es requerido")
-	c.Validation.Required(last_name).Message("Apellido es requerido")
-	c.Validation.Required(ci).Message("Apellido es requerido")
-	c.Validation.Required(ci).Message("Apellido es requerido")
-	c.Validation.Required(birthday).Message("Fecha de nacimiento es requerido")
-	c.Validation.Required(sex).Message("Sexo es requerido")
-	c.Validation.MinSize(sex, 1).Message("Debe ingresar solamente las iniciales")
-
-	if c.Validation.HasErrors() {
-		c.Validation.Keep()
-		c.FlashParams()
-		return c.Redirect(Client.Index)
-	}
-
-	const SQL_DATE_FORMAT = "2006-01-02"
-	t, _ := time.Parse(SQL_DATE_FORMAT, birthday)
-
-	client := models.Client{
-		First_name: first_name,
-		Last_name: last_name,
-		Ci: ci,
-		Birthday: t,
-		Sex: sex,
-	}
-	res := DB.Create(&client)
-
-	if res.Error != nil {
-		return c.RenderError(errors.New("Record Create failure." + res.Error.Error()))
-	}
-	return c.Redirect("/clients")
-}
-
-func (c Client) Delete() revel.Result {
-
-	id := c.Params.Route.Get("id")
-	client := []models.Client{}
-	res := DB.Delete(&client, id)
-	// res := DB.Unscoped().Delete(&clients, id) // delete permanently
-
-	if res.Error != nil {
-		return c.RenderError(errors.New("Record Delete failure." + res.Error.Error()))
-	}
-
-	return c.Redirect("/clients")
-}
-
-func (c Client) Edit(id int) revel.Result {
-	h2 := "GO CRUD WITH POSTGRES - EDIT RECORD"
-	client := []models.Client{}
-	res := DB.Find(&client, id)
-	err := res.Error
-
-	if err != nil {
-		return c.RenderError(errors.New("Record not Found"))
-	}
-	return c.Render(client, h2)
-}
-
-func (c Client) Update() revel.Result {
-	
-	pk := c.Params.Form.Get("id")
-	id, _ := strconv.Atoi(pk)
-	
-	const SQL_DATE_FORMAT = "2006-01-02"
-
-	first_name := c.Params.Form.Get("first_name")
-	last_name := c.Params.Form.Get("last_name")
-	ci := c.Params.Form.Get("ci")
-	birthday := c.Params.Form.Get("birthday")
-	sex := c.Params.Form.Get("sex")
-
-	c.Validation.Required(first_name).Message("Nombre es requerido")
-	c.Validation.Required(last_name).Message("Apellido es requerido")
-	c.Validation.Required(ci).Message("Documento es requerido")
-	c.Validation.Required(birthday).Message("Fecha de nacimiento es requerido")
-	c.Validation.Required(sex).Message("Sexo es requerido")
-	c.Validation.MinSize(sex, 1).Message("Debe ingresar solamente las iniciales")
-	
-	t, _ := time.Parse("2006-01-02", birthday)
-	fmt.Println("Time form:", t)
-
-
-	if c.Validation.HasErrors() {
-		c.Validation.Keep()
-		c.FlashParams()
-		return c.Redirect(Client.List)
-	}
-
-	client := models.Client{}
-	// res := DB.Model(&client).Where("id = ?", id).Update("first_name", first_name)
-	// res := DB.Model(&client).Where("id = ?", id).Updates(map[string]interface{}{"first_name": first_name, "last_name": last_name, "ci": ci})
-	res := DB.Model(&client).Where("id = ?", id).Updates(models.Client{First_name: first_name, Last_name: last_name, Ci: ci, Birthday: t, Sex: sex})
-
-	if res.Error != nil {
-		return c.RenderError(errors.New("Record Create failure." + res.Error.Error()))
-	}
-	return c.Redirect("/clients")
-}
-
-func (c Client) Search() revel.Result {
-	h2 := "GO CRUD WITH POSTGRES - SEARCH RECORD"
-	notFound := "No existe registro"
-	client := []models.Client{}
-	first_name := c.Params.Form.Get("first_name")
-	last_name := c.Params.Form.Get("last_name")
-	document := c.Params.Form.Get("document")
-
-	if first_name == "" && last_name == "" && document == ""{
-		res := DB.Find(&client)
-		err := res.Error
-
-		if err != nil {
-			return c.RenderError(errors.New("Record not Found"))
-		}
-		// return c.Render(client, h2)
-		
-	} else if first_name == "" && last_name == "" {
-		res := DB.Where("ci LIKE ?", document+"%").Find(&client)
-		// res := DB.Find(&client, "ci LIKE ?", document)
-		err := res.Error
-
-		if err != nil {
-			return c.RenderError(errors.New("Record not Found"))
-		}
-		// return c.Render(client, h2)
-	} else if last_name == "" && document == "" {
-		res := DB.Where("first_name LIKE ?", first_name+"%").Find(&client)
-		err := res.Error
-
-		if err != nil {
-			return c.RenderError(errors.New("Record not Found"))
-		}
-		// return c.Render(client, h2)
-	} else if first_name == "" && document == "" {
-		res := DB.Where("last_name LIKE ?", last_name+"%").Find(&client)
-		err := res.Error
-
-		if err != nil {
-			return c.RenderError(errors.New("Record not Found"))
-		}
-		// return c.Render(client, h2)
-	} else if first_name == "" {
-		res := DB.Where("last_name LIKE ? OR ci LIKE ?", last_name+"%", document+"%").Find(&client)
-		err := res.Error
-
-		if err != nil {
-			return c.RenderError(errors.New("Record not Found"))
-		}
-		// return c.Render(client, h2)
-	} else if last_name == "" {
-		res := DB.Where("first_name LIKE ? OR ci LIKE ?", first_name, document).Find(&client)
-		err := res.Error
-
-		if err != nil {
-			return c.RenderError(errors.New("Record not Found"))
-		}
-		// return c.Render(client, h2)
-	} else if document == "" {
-		res := DB.Where("first_name LIKE ? OR last_name LIKE ?", first_name, last_name).Find(&client)
-		err := res.Error
-
-		if err != nil {
-			return c.RenderError(errors.New("Record not Found"))
-		}
-		// return c.Render(client, h2)
-	}
-
-	return c.Render(client, h2, notFound)
-	// db.Where("updated_at > ?", lastWeek).Find(&users)
 }
--- app/models/client.go
+++ app/models/client.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	// "fmt"
+	"github.com/revel/revel"
 	"gorm.io/gorm"
 	"time"
 )
@@ -11,7 +12,7 @@ type Client struct {
 	First_name string `sql:"size:255" json:"first_name"`
 	Last_name string `sql:"size:255" json:"last_name"`
 	Ci string `sql:"size:20" json:"ci"`
-	Birthday time.Time `sql:"timestamptz" json:"birthday"`
+	Birthday time.Time `sql:"date" json:"birthday`
 	Sex string `sql:size:1 json:"sex"`
 	Nationality string `sql:size:140 json:"nationality"`
 	Des_type string `sql:size:140 json: "des_type"`
@@ -22,6 +23,19 @@ type Client struct {
 	Phone string `sql:size:10 json:"phone"`
 }
 
+func (c Client) BirthdayDateStr() string {
+	return c.Birthday.Format("2006-01-02")
+}
+
+func (client *Client) Validate(v *revel.Validation) {
+	v.Required(client.First_name).Message("* El nombre es requerido")
+	v.Required(client.Last_name).Message("* El apellido es requerido")
+	v.Required(client.Ci).Message("* La cedula es requerido")
+	v.Required(client.Birthday).Message("* La fecha de nacimiento es requerido")
+	v.Required(client.Sex).Message("* El sexo es requerido")
+	v.MaxSize(client.Sex, 1).Message("* Ingrese solo la inicial")
+}
+
 // const (
 // 	DATA_FORMAT = "Jan _2, 2006"
 // 	SQL_DATE_FORMAT = "2006-01-02"
@@ -30,7 +44,3 @@ type Client struct {
 // func (c Client) formtDate() string {
 // 	return fmt.Sprintf("%s", c.Birthday.Format(SQL_DATE_FORMAT))
 // }
-
-func (c Client) BirthdayDateStr() string {
-	return c.Birthday.Format("2006-01-02")
-}
